d206d0db204f543392b25f7349d94f76faeb30b5
Austin <austin.platt@distilled.co.uk>
2014-08-29 11:02:14 +0100

    use web-mode for html files

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,13 +1,28 @@
-(require-package 'tidy)
-(add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
+;; (require-package 'tidy)
+;; (add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
 
-(require-package 'tagedit)
-(after-load 'sgml-mode
-  (tagedit-add-paredit-like-keybindings)
-  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
+;; (require-package 'tagedit)
+;; (after-load 'sgml-mode
+;;   (tagedit-add-paredit-like-keybindings)
+;;   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
-(add-auto-mode 'html-mode "\\.(jsp|tmpl)\\'")
+;; (add-auto-mode 'html-mode "\\.(jsp|tmpl)\\'")
 
-;; Note: ERB is configured in init-ruby-mode
+;; ;; Note: ERB is configured in init-ruby-mode
+
+(require-package 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-engines-alist
+          '(("django" . "/labsmanager/.*\\.html\\'")
+            ("django" . "/distilledstore/.*\\.html\\'"))
+          )
+
+(setq web-mode-extra-snippets
+      '(
+        ("django" . (("include" . ("{% include \""  . "\" %}"))))
+       ))
+
 
 (provide 'init-html)
