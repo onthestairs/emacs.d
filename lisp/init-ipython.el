213f1102ed6cbce6432565f7b2d1c9c5e6fccfd3
Austin <austin.platt@distilled.co.uk>
2014-12-01 13:41:04 +0000

    add jedi mode

--- lisp/init-ipython.el
+++ lisp/init-ipython.el
@@ -2,4 +2,10 @@
 
 (setq ein:use-auto-complete t)
 
+(require-package 'deferred)
+(require-package 'jedi)
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
 (provide 'init-ipython)
